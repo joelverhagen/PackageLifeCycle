9d10a3d396a0b898db6ce583fa2abf630a41bd3a
Simon Cropp <simon.cropp@gmail.com>
2024-05-30 07:39:03 +1000

    verify and mdsnippets (#1)
    * verify and mdsnippets
    
    * Delete AssemblyAttributes.cs
    
    * .
    
    * Update .gitattributes

--- src/NuGet.PackageLifeCycle/Program.cs
+++ src/NuGet.PackageLifeCycle/Program.cs
@@ -9,34 +9,40 @@ using System.CommandLine.Hosting;
 using System.CommandLine.Parsing;
 using System.Runtime.InteropServices;
 
-var builder = new UserAgentStringBuilder("NuGet.PackageLifeCycle")
-    .WithOSDescription(string.Join("; ", new object[]
-    {
-        RuntimeInformation.OSDescription,
-        RuntimeInformation.OSArchitecture,
-        RuntimeInformation.FrameworkDescription,
-        RuntimeInformation.RuntimeIdentifier,
-    }));
-UserAgent.SetUserAgentString(builder);
-
-var runner = new CommandLineBuilder(new PackageLifeCycleCommand())
-    .UseExceptionHandler()
-    .UseHost(_ => Host.CreateDefaultBuilder(args), (builder) => builder
-        .UseSerilog()
-        .ConfigureServices((hostContext, services) =>
-        {
-            services.AddTransient<DeprecationService>();
-            services.AddCustomSerilog();
-        })
-        .UseCommandHandler<DeprecateCommand, DeprecateCommand.Handler>())
-    .UseHelp()
-    .UseVersionOption()
-    .UseParseErrorReporting()
-    .Build();
-
-if (args.Length == 0 || args.All(string.IsNullOrWhiteSpace))
+public static class Program
 {
-    args = ["--help"];
-}
+    public static async Task<int> Main(string[] args)
+    {
+        var builder = new UserAgentStringBuilder("NuGet.PackageLifeCycle")
+            .WithOSDescription(string.Join("; ", new object[]
+            {
+                RuntimeInformation.OSDescription,
+                RuntimeInformation.OSArchitecture,
+                RuntimeInformation.FrameworkDescription,
+                RuntimeInformation.RuntimeIdentifier,
+            }));
+        UserAgent.SetUserAgentString(builder);
 
-return await runner.Parse(args).InvokeAsync();
+        var runner = new CommandLineBuilder(new PackageLifeCycleCommand())
+            .UseExceptionHandler()
+            .UseHost(_ => Host.CreateDefaultBuilder(args), (builder) => builder
+                .UseSerilog()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddTransient<DeprecationService>();
+                    services.AddCustomSerilog();
+                })
+                .UseCommandHandler<DeprecateCommand, DeprecateCommand.Handler>())
+            .UseHelp()
+            .UseVersionOption()
+            .UseParseErrorReporting()
+            .Build();
+
+        if (args.Length == 0 || args.All(string.IsNullOrWhiteSpace))
+        {
+            args = ["--help"];
+        }
+
+        return await runner.Parse(args).InvokeAsync();
+    }
+}
